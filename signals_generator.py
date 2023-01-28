52a245ff97b092849fed5d3c4edcf1cf1c3698fd
Sam <123043158+Speedo182@users.noreply.github.com>
2023-01-29 10:44:41 +1100

    Update signals_generator.py

--- signals_generator.py
+++ signals_generator.py
@@ -1,33 +1,77 @@
-import coin_data_scraper
-import news_data_scraper
-import config
+import pandas as pd
+import talib
+import telegram
 
-def generate_signals(coin_list):
-    signals = {}
-    for coin in coin_list:
-        coin_data = coin_data_scraper.scrape_data(coin)
-        news_data = news_data_scraper.scrape_news(coin)
+def generate_signals(data, social_media_data, models):
+    # load the social media data
+    social_media_sentiments = pd.read_csv(social_media_data)
 
-        # analyze data and generate signals
-        current_price = coin_data["price"]
-        volume = coin_data["volume"]
-        news_sentiment = analyze_news_sentiment(news_data)
-        historical_data = analyze_historical_data(coin_data)
+    # calculate the overall sentiment
+    overall_sentiment = social_media_sentiments["sentiment"].mean()
 
-        signal = None
-        margin = config.min_margin
-        if current_price * volume > news_sentiment + historical_data and margin > 5:
-            signal = "BUY"
-        elif current_price * volume < news_sentiment + historical_data and margin < 100:
-            signal = "SELL"
+    # create an empty list to store the signals
+    signals = []
+
+    # loop through the crypto coins
+    for coin in data.columns:
+        # load the historical data for the coin
+        coin_data = data[coin]
+
+        # calculate the technical indicators
+        coin_data["SMA"] = talib.SMA(coin_data["close"])
+        coin_data["RSI"] = talib.RSI(coin_data["close"])
+        coin_data["MACD"], coin_data["MACD_signal"], coin_data["MACD_hist"] = talib.MACD(coin_data["close"]
+        coin_data["FIB_RETRACEMENT"] = talib.FIBONACCI(coin_data["high"], coin_data["low"], coin_data["close"])
+        coin_data["ELLIOTT_WAVE"] = talib.ELLIOTT(coin_data["close"])
+
+        # make predictions using the models
+        random_forest_prediction = models["random_forest"].predict(coin_data[["open", "high", "low", "volume", "SMA", "RSI", "MACD", "MACD_signal", "MACD_hist", "FIB_RETRACEMENT", "ELLIOTT_WAVE"]])[0]
+        svm_prediction = models["svm"].predict(coin_data[["open", "high", "low", "volume", "SMA", "RSI", "MACD", "MACD_signal", "MACD_hist", "FIB_RETRACEMENT", "ELLIOTT_WAVE"]])[0]
+        neural_network_prediction = models["neural_network"].predict(coin_data[["open", "high", "low", "volume", "SMA", "RSI", "MACD", "MACD_signal", "MACD_hist", "FIB_RETRACEMENT", "ELLIOTT_WAVE"]])[0]
+
+        # calculate the average prediction
+        prediction = (random_forest_prediction + svm_prediction + neural_network_prediction) / 3
+
+        # check if the sentiment is positive
+        if overall_sentiment > 0:
+            # check if the prediction is higher than the current price
+            if prediction > coin_data["close"][-1]:
+                # generate a buy signal
+                signals.append({"coin": coin, "signal": "buy", "price": prediction})
+            else:
+                # generate a sell signal
+                signals.append({"coin": coin, "signal": "sell", "price": prediction})
         else:
-            signal = "HOLD"
+            # check if the prediction is lower than the current price
+            if prediction < coin_data["close"][-1]:
+                # generate a buy signal
+                signals.append({"coin": coin, "signal": "buy", "price": prediction})
+            else:
+                # generate a sell signal
+                signals.append({"coin": coin, "signal": "sell", "price": prediction})
 
-        signals[coin] = signal
-
-    return signals
+    # send the signals to the Telegram group
+    bot = telegram.Bot(token="YOUR_TELEGRAM_BOT_TOKEN")
+    for signal in signals:
+        bot.send_message(chat_id="YOUR_TELEGRAM_GROUP_CHAT_ID", text="Signal for {}: {} at price {}".format(signal["coin"], signal["signal"], signal["price"]))
 
 if __name__ == "__main__":
-    coin_list = ["BTC", "ETH", "XRP", "LTC", "BCH", "BNB", "EOS", "BSV", "XLM", "ADA", "TRX", "LINK", "DOT", "XTZ", "YFI", "AAVE", "UNI", "DOGE", "SUSHI", "CRV"]
-    signals = generate_signals(coin_list)
-    print(signals)
+    # load the historical data
+    data = pd.read_csv("historical_data.csv")
+
+    # select the relevant columns
+    data = data[["open", "high", "low", "close", "volume"]]
+
+    # load the models
+    import pickle
+    with open("random_forest_model.pkl", "rb") as f:
+        random_forest_model = pickle.load(f)
+    with open("svm_model.pkl", "rb") as f:
+        svm_model = pickle.load(f)
+    with open("neural_network_model.pkl", "rb") as f:
+        neural_network_model = pickle.load(f)
+    models = {"random_forest": random_forest_model, "svm": svm_model, "neural_network": neural_network_model}
+
+    # generate the signals
+    generate_signals(data, "social_media_sentiments.csv", models)
+
