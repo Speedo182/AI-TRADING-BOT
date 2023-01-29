d47d3c1939fe24519764ae76c82a09ab1e6f863d
Command Church <123043158+Speedo182@users.noreply.github.com>
2023-01-29 13:06:00 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
+import requests
+import json
+import random
+import datetime
+import time
+from datetime import timedelta
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Telegram AI Trading bot. Please type /help for a list of commands.")
+
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Commands: \n/start - start the bot \n/help - view a list of commands \n/current_price - view the current price of a specific stock \n/historical_data - view historical data of a specific stock \n/predict - view predicted future price of a specific stock")
+
+def current_price(update, context):
+    stock_ticker = update.message.text.split()[1]
+    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + stock_ticker + "&apikey=YOUR_API_KEY"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    try:
+        current_price = data["Global Quote"]["05. price"]
+        context.bot.send_message(chat_id=update.effective_chat.id, text="The current price of " + stock_ticker + " is " + current_price)
+    except:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid stock ticker. Please try again.")
+
+def historical_data(update, context):
+    stock_ticker = update.message.text.split()[1]
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + stock_ticker + "&apikey=YOUR_API_KEY"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    try:
+        dates = list(data["Time Series (Daily)"].keys())
+        prices = []
+        for date in dates:
+            price = data["Time Series (Daily)"][date]["4. close"]
+            prices.append(float(price))
+        df = pd.DataFrame({'date':dates, 'price':prices})
+        df['date'] = pd.to_datetime(df['date'])
+        df.set_index('date', inplace=True)
+        plt.figure(figsize=(16,8))
+        sns.lineplot(data=df)
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.title("HistoricalPrice Plot")
+        plt.legend()
+        plt.show()
+        def telegram_bot(self):
+    updater = Updater(token='YOUR_TOKEN', use_context=True)
+    dp = updater.dispatcher
+    
+    def start(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the AI Trading Bot. Please enter a stock symbol (e.g. AAPL) to begin.")
+    
+    def stock(update, context):
+        symbol = update.message.text
+        try:
+            self.get_data(symbol)
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('stock_plot.png', 'rb'))
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"The current price of {symbol} is {self.current_price}")
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"The current moving average of {symbol} is {self.moving_average}")
+        except:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid stock symbol. Please enter a valid stock symbol.")
+    
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text, stock))
+    
+    updater.start_polling()
+    updater.idle()
+    if name == "main":
+bot = TradingBot()
+bot.telegram_bot()
