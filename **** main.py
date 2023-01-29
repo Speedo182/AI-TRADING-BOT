8993fbda79aaaf94fba630d138aa16820b4719a9
Command Church <123043158+Speedo182@users.noreply.github.com>
2023-01-29 13:55:42 +1100

    Update **** main.py

--- **** main.py
+++ **** main.py
@@ -2,7 +2,7 @@
 Also note that this code assumes that you have already imported the necessary libraries (e.g. pandas, matplotlib, etc.) and 
 have properly set up your python environment.
 
-
+A review of this code is required. 
 
 
 import os
@@ -19,66 +19,144 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Function to handle the start command
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Telegram AI Trading bot. Please type /help for a list of commands.")
+    update.message.reply_text("Welcome to the bot! Type /help to see a list of commands.")
 
-def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Commands: \n/start - start the bot \n/help - view a list of commands \n/current_price - view the current price of a specific stock \n/historical_data - view historical data of a specific stock \n/predict - view predicted future price of a specific stock")
+# Function to handle the help command
+def help_command(update, context):
+    update.message.reply_text("Commands: \n /start - start the bot \n /help - display a list of commands \n /price - display the current price of a cryptocurrency \n /plot - display a historical price plot of a cryptocurrency \n /subscribe - subscribe to receive updates on a cryptocurrency \n /unsubscribe - unsubscribe from receiving updates on a cryptocurrency \n /loyalty - check loyalty program status and rewards \n /affiliate - check affiliate program status and commissions \n /free_trial - check free trial status and progress on sweep widget tasks \n /giveaway - check monthly giveaway status and entry")
 
-def current_price(update, context):
-    stock_ticker = update.message.text.split()[1]
-    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + stock_ticker + "&apikey=YOUR_API_KEY"
+# Function to handle the price command
+def price(update, context):
+    # Get the cryptocurrency ticker from the user's message
+    ticker = context.args[0]
+    # Make a GET request to the CoinGecko API to get the current price of the cryptocurrency
+    url = "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd".format(ticker)
     response = requests.get(url)
     data = json.loads(response.text)
-    try:
-        current_price = data["Global Quote"]["05. price"]
-        context.bot.send_message(chat_id=update.effective_chat.id, text="The current price of " + stock_ticker + " is " + current_price)
-    except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid stock ticker. Please try again.")
+    # Extract the current price from the API response
+    current_price = data[ticker]["usd"]
+    update.message.reply_text("The current price of {} is ${}".format(ticker, current_price))
 
-def historical_data(update, context):
-    stock_ticker = update.message.text.split()[1]
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + stock_ticker + "&apikey=YOUR_API_KEY"
+# Function to handle the plot command
+def plot(update, context):
+    # Get the cryptocurrency ticker from the user's message
+    ticker = context.args[0]
+    # Make a GET request to the CoinGecko API to get the historical price data of the cryptocurrency
+    url = "https://api.coingecko.com/api/v3/coins/{}/market_chart?vs_currency=usd&days=365".format(ticker)
     response = requests.get(url)
     data = json.loads(response.text)
-    try:
-        dates = list(data["Time Series (Daily)"].keys())
-        prices = []
-        for date in dates:
-            price = data["Time Series (Daily)"][date]["4. close"]
-            prices.append(float(price))
-        df = pd.DataFrame({'date':dates, 'price':prices})
-        df['date'] = pd.to_datetime(df['date'])
-        df.set_index('date', inplace=True)
-        plt.figure(figsize=(16,8))
-        sns.lineplot(data=df)
-        plt.xlabel("Date")
-        plt.ylabel("Price")
-        plt.title("HistoricalPrice Plot")
-        plt.legend()
-        plt.show()
-        def telegram_bot(self):
-    updater = Updater(token='YOUR_TOKEN', use_context=True)
-    dp = updater.dispatcher
-    
-    def start(update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the AI Trading Bot. Please enter a stock symbol (e.g. AAPL) to begin.")
-    
-    def stock(update, context):
-        symbol = update.message.text
-        try:
-            self.get_data(symbol)
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('stock_plot.png', 'rb'))
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"The current price of {symbol} is {self.current_price}")
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"The current moving average of {symbol} is {self.moving_average}")
-        except:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid stock symbol. Please enter a valid stock symbol.")
-    
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text, stock))
-    
-    updater.start_polling()
-    updater.idle()
-    if name == "main":
-bot = TradingBot()
-bot.telegram_bot()
+    # Extract the historical price data from the API response
+    prices = data["prices"]
+    # Create a pandas DataFrame from the historical price data
+    df = pd.DataFrame(prices, columns=["date", "price"])
+    # Convert the date column to datetime format
+    df["date"] = pd.to_datetime(df["date"], unit='ms')
+    # Set the date column as the index
+    df.set_index("date", inplace=True)
+    # Create a new column for the moving average
+df["moving_average"] = df["price"].rolling(window=14).mean()
+
+# Create a new column for the upper Bollinger Band
+df["upper_band"] = df["moving_average"] + (df["price"].rolling(window=14).std() * 2)
+
+# Create a new column for the lower Bollinger Band
+df["lower_band"] = df["moving_average"] - (df["price"].rolling(window=14).std() * 2)
+
+# Create a new column for the signal
+df["signal"] = 0
+
+# Iterate through the dataframe and set the signal based on the Bollinger Bands
+for i in range(len(df)):
+    if df["price"][i] > df["upper_band"][i]:
+        df["signal"][i] = 1
+    elif df["price"][i] < df["lower_band"][i]:
+        df["signal"][i] = -1
+
+# Plot the data
+plt.figure(figsize=(18,9))
+plt.plot(df["price"], label="Price")
+plt.plot(df["moving_average"], label="Moving Average")
+plt.plot(df["upper_band"], label="Upper Band")
+plt.plot(df["lower_band"], label="Lower Band")
+plt.legend(loc="best")
+plt.ylabel("Price")
+plt.title("Historical Price with Bollinger Bands and Signals")
+plt.show()
+
+# Create a new dataframe for the signals
+signals = pd.DataFrame(columns=["date", "signal"])
+
+# Iterate through the dataframe and add the signals to the new dataframe
+for i in range(len(df)):
+    if df["signal"][i] != 0:
+        signals = signals.append({"date": df.index[i], "signal": df["signal"][i]}, ignore_index=True)
+
+# Return the signals dataframe
+return signals
+
+# Define a function to handle the /signals command
+def signals(update, context):
+# Get the user's message
+message = update.message.text
+# Get the stock symbol from the message
+stock_symbol = message.split(" ")[1]
+
+# Get the historical data for the stock
+df = get_data(stock_symbol)
+
+# Get the signals for the stock
+signals = get_signals(df)
+
+# Send the signals to the user
+context.bot.send_message(chat_id=update.effective_chat.id, text=signals.to_string())
+#Define a function to handle the /start command
+def start(update, context):
+# Send a message to the user
+context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Stock Bot! Use the /signals command to get trading signals for a stock. Example: /signals
+                         
+                        AAPL
+                         def signals(update, context):
+stock_ticker = context.args[0]
+try:
+# Get historical data for the stock
+df = pd.read_csv(f"{stock_ticker}.csv")
+                             # Set the date column as the index
+    df.set_index("Date", inplace=True)
+
+    # Calculate the moving average
+    df["50_MA"] = df["Close"].rolling(window=50).mean()
+    df["200_MA"] = df["Close"].rolling(window=200).mean()
+
+    # Identify when the 50 MA crosses above the 200 MA
+    df["Signal"] = np.where(df["50_MA"] > df["200_MA"], 1, 0)
+
+    # Identify when the signal changes
+    df["Position"] = df["Signal"].diff()
+
+    # Create a column to hold the stock returns
+    df["Returns"] = df["Close"].pct_change()
+
+    # Calculate the strategy returns
+    df["Strategy_Returns"] = df["Position"].shift() * df["Returns"]
+
+    # Plot the returns
+    plt.plot(df["Strategy_Returns"].cumsum())
+    plt.xlabel("Date")
+    plt.ylabel("Strategy Returns (%)")
+    plt.title(f"{stock_ticker} Trading Strategy Returns")
+    plt.show()
+
+    # Send the chart to the user
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open("chart.png", "rb"))
+
+except FileNotFoundError:
+    update.message.reply_text("Invalid stock ticker. Please enter a valid stock ticker.")
+# Add the signals command to the bot
+signals_handler = CommandHandler("signals", signals)
+dispatcher.add_handler(signals_handler)
+
+# Start the bot
+updater.start_polling()
+updater.idle()
